65d12b15f9e8342e914ebdc2cfea371449f5f718
Patrice <Patrice@1476686f-be1a-4720-a652-3e6095220f58>
2014-05-07 09:57:14 +0000

    surcharge bouton impression devis

--- sale.py
+++ sale.py
@@ -102,6 +102,20 @@ class sale_order(osv.osv):
         
         return True
     
+    def print_quotation(self, cr, uid, ids, context=None):
+        '''
+        This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time'
+        wf_service = netsvc.LocalService("workflow")
+        wf_service.trg_validate(uid, 'sale.order', ids[0], 'quotation_sent', cr)
+        datas = {
+                 'model': 'sale.order',
+                 'ids': ids,
+                 'form': self.read(cr, uid, ids[0], context=context),
+        }
+        return {'type': 'ir.actions.report.xml', 'report_name': 'simpac_report_sale', 'datas': datas, 'nodestroy': True}
+    
     
     
 class sale_order_line(osv.osv):
